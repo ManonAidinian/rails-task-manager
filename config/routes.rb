53766a0b943bf21fa2eae44d7779edfa1aba59eb
manonaidinian <manonaidinian@gmail.com>
2022-05-24 16:55:38 +0800

    finishing the exrecise

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #index
-get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index'
 
-# show
-get '/tasks/:id', to: 'tasks#show'
+  # new / create
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
 
-# new / create
-get '/tasks/new', to: 'tasks#new'
-post '/tasks', to: 'tasks#create'
+  # show
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
 
-# edit / update
-get '/tasks/:id/edit', to: 'tasks#edit'
-patch '/tasks/:id', to: 'tasks#update'
+  # edit / update
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch '/tasks/:id', to: 'tasks#update'
 
-#destroy
-delete '/tasks/:id', to: 'tasks#destroy'
+  #destroy
+  delete '/tasks/:id', to: 'tasks#destroy', as: 'delete'
 
 # resources :restaurants
 end
